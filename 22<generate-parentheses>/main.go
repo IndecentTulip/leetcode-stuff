0f1a37fb104a5eff6766e9ac87feaef82e0b3977
Vul <aleksandrbabenko02@gmail.com>
2024-04-30 16:53:10 -0700

    made 22

--- 22<generate-parentheses>/main.go
+++ 22<generate-parentheses>/main.go
@@ -1,15 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 )
 
 func generateParenthesis(n int) []string {
 
+  stack := []string{}
+  result := []string{}
+  var backtrack func(int, int)
 
-  return  nil
+  backtrack = func(open int, closed int)  {
+    //fmt.Println("open", open, "closed", closed, "stack", stack, "result", result)
+
+    if open == closed && closed == n {
+      result = append(result, strings.Join(stack, ""))
+      return
+    }
+    if open < n {
+      stack = append(stack, "(")
+      backtrack(open+1, closed)
+      stack = stack[:len(stack)-1]
+    }
+    if closed < open{
+      stack = append(stack, ")")
+      backtrack(open, closed+1)
+      stack = stack[:len(stack)-1]
+    }
+
+  }
+  backtrack(0,0)
+  return result
 }
 
 
 func main(){
 
+  fmt.Println(generateParenthesis(3))
 }
